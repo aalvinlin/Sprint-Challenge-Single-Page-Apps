b4cd899eff713ed1aa77313c4d932d8c1be9c759
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2019-12-20 08:33:46 -0800

    search form added, styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
-import Header from "./components/Header.js";
+import {Route} from "react-router-dom"
 
+import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage";
+
+import SearchForm from "./components/SearchForm";
+
+import CharacterList from "./components/CharacterList";
+import LocationsList from "./components/LocationsList";
 
 export default function App() {
   return (
     <main>
       <Header />
+      
+      <SearchForm />
+
+      <Route exact path="/">
+        <WelcomePage />
+      </Route>
+
+      <Route path="/characters">
+        <CharacterList />
+      </Route>
+
+      <Route exact path="/locations">
+        <LocationsList />
+      </Route>
+
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,60 @@
 import React, { useState } from "react";
 
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+`;
+
+const SearchInstructions = styled.h3`
+
+  font-size: 1rem;
+
+`;
+
+const SearchInput = styled.input`
+
+  font-size: 2rem;
+
+  line-height: 4rem;
+  padding: 0 1rem;
+
+`;
+
+const SubmitButton = styled.button`
+
+  font-size: 2rem;
+
+  line-height: 4rem;
+  padding: 0 1rem;
+
+`;
+
+const handleSubmit = function (event) {
+
+  event.preventDefault();
+
+}
+
 export default function SearchForm() {
  
   return (
     <section className="search-form">
-     // Add a search form here
+     
+      <StyledForm name="search" action={handleSubmit}>
+
+        <SearchInstructions>Search for a character or episode...</SearchInstructions>
+
+        <SearchInput type="text"/>
+
+        <SubmitButton type="submit">Search!</SubmitButton>
+
+      </StyledForm>
+
     </section>
   );
 }
